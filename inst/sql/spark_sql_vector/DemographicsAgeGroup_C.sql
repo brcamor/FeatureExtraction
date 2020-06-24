6d6570ff9af5b07d9ab77aa12346cf9077575658
Benjamin Amor <bamor@palantir.com>
2020-06-24 16:52:08 -0400

    picking up 3.0 sql changes

--- inst/sql/spark_sql_vector/DemographicsAgeGroup_C.sql
+++ inst/sql/spark_sql_vector/DemographicsAgeGroup_C.sql
@@ -2,9 +2,9 @@
 SELECT covariate_id,
 	CAST(CONCAT (
 		'age group: ',
-		RIGHT(CONCAT('00', CAST(5 * (covariate_id - @analysis_id) / 1000 AS INT)), 2),
-		'-',
-		RIGHT(CONCAT('00', CAST((5 * (covariate_id - @analysis_id) / 1000) + 4 AS INT)), 2)
+		RIGHT(CONCAT('   ', CAST(5 * (covariate_id - @analysis_id) / 1000 AS INT)), 3),
+		' - ',
+		RIGHT(CONCAT('   ', CAST((5 * (covariate_id - @analysis_id) / 1000) + 4 AS INT)), 3)
 		) AS VARCHAR(512)) AS covariate_name,
 	@analysis_id AS analysis_id,
 	0 AS concept_id
